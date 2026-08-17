1c0e1bbdf3bcf821b97cb959a2212d3fe03f1b2a
vycostaa <vycosta@icloud.com>
2026-08-17 09:01:42 -0400

    Replace OSS setup with enterprise setup in install script

--- install/pangolin-install.sh
+++ install/pangolin-install.sh
@@ -47,7 +47,7 @@ cd /opt/pangolin
 mkdir -p /opt/pangolin/config/{traefik,db,letsencrypt,logs}
 $STD npm ci
 $STD npm run set:pg
-$STD npm run set:oss
+$STD npm run set:enterprise
 rm -rf server/private
 DATABASE_URL="postgresql://pangolin:${PG_DB_PASS}@localhost:5432/pangolin" $STD npm run db:generate
 $STD npm run build
